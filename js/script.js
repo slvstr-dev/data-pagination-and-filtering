c79f64ce680a236abdcc8195ee99babc5e00ec5b
Sylvester Hofstra <sylvesterhofstra@MacBook-Air-van-Sylvester.local>
2020-11-16 12:15:02 +0100

    Added initial logic for showing pages with pagination

--- js/script.js
+++ js/script.js
@@ -1,30 +1,63 @@
-/*
-Treehouse Techdegree:
-FSJS Project 2 - Data Pagination and Filtering
-*/
+const itemsPerPage = 9;
 
+const showPage = (list, page) => {
+   let startIndex = (page * itemsPerPage) - itemsPerPage;
+   const endIndex = (page * itemsPerPage);
+   let studentList = document.querySelector(".student-list");
 
+   studentList.innerHTML = "";
 
-/*
-For assistance:
-   Check out the "Project Resources" section of the Instructions tab: https://teamtreehouse.com/projects/data-pagination-and-filtering#instructions
-   Reach out in your Slack community: https://treehouse-fsjs-102.slack.com/app_redirect?channel=unit-2
-*/
+   for (let i = startIndex; i < endIndex; i++) {
+      const studentItem = ` 
+         <li class="student-item cf">
+            <div>
+               <img src="${list[i].picture.large}" class="avatar" alt="Profile picture"/>
 
+               <h3>${list[i].name.first} ${list[i].name.last}</h3>
 
+               <span class="email">${list[i].email}</span>
+            </div>
 
-/*
-Create the `showPage` function
-This function will create and insert/append the elements needed to display a "page" of nine students
-*/
+            <div class="joined-details">
+               <span class="date">Joined ${list[i].registered.date}</span>
+            </div>
+         </li>
+      `;
 
+      studentList.insertAdjacentHTML("beforeend", studentItem);
+   };
+};
 
+const addPagination = list => {
+   const totalPages = Math.floor(list.length / itemsPerPage);
+   const linkList = document.querySelector(".link-list");
 
-/*
-Create the `addPagination` function
-This function will create and insert/append the elements needed for the pagination buttons
-*/
+   linkList.innerHTML = "";
 
+   for (let i = 1; i < totalPages; i++) {
+      const button = `
+         <li>
+            <button type="button">${i}</button>
+         </li>
+      `;
 
+      linkList.insertAdjacentHTML('beforeend', button);
+   }
 
-// Call functions
+   let activeButton = document.querySelectorAll("[type='button']")[0];
+
+   activeButton.classList.add("active");
+
+   linkList.addEventListener("click", e => {
+      if (e.target.tagName === "BUTTON") {
+         activeButton.classList.remove("active");
+         e.target.classList.add("active");
+         activeButton = e.target;
+
+         showPage(data, e.target.innerText);
+      }
+   });
+};
+
+showPage(data, 1);
+addPagination(data);
